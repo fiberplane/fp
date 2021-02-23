1bf252a676e26ad3207301ec2c6bdd17d05e4657
= <mies@fiberplane.com>
2021-02-23 15:36:56 +0100

    WIP Clap arg and subcommand parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use clap::{App, Arg, ArgMatches};
+
 fn main() {
+    let matches = App::new("Fiberplane CLI")
+        .version("0.1")
+        .author("The Fiberplane Team")
+        .about("Interacts with the Fiberplane API")
+        .subcommand(
+            App::new("webhook")
+                .about("Interact with Fiberplane Webhooks")
+                .arg(
+                    Arg::new("label")
+                        .short('l')
+                        .long("label")
+                        .multiple(true)
+                        .about("Sets the alert labels"),
+                )
+                .arg(
+                    Arg::new("annotation")
+                        .short('a')
+                        .long("annotation")
+                        .multiple(true)
+                        .about("Set the alert annotations"),
+                ),
+        )
+        .get_matches();
+
+    println!("{:?}", matches);
+
+    // match app_m.subcommand() {
+    //     ("clone",  Some(sub_m)) => {}, // clone was used
+    //     ("push",   Some(sub_m)) => {}, // push was used
+    //     ("commit", Some(sub_m)) => {}, // commit was used
+    //     _                       => {}, // Either no subcommand or one not tested for...
+    // }
+
+
+    match matches.subcommand() {
+        Some(("webhook", sub_m)) => handle_webhook(sub_m),
+        _ => panic!(),
+    }
+
+    // read config file
+
+    // create trigger crud
+
+    // 1) read cli args
+    // 2) (turn into alert)
+    // 3) do POST on api/webhooks/
     println!("Hello, world!");
+}
+
+fn handle_webhook(matches: &ArgMatches) {
+
+
 }
