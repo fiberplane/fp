4bc9bcb5df45b0aedeeff42e87bff7d6cf8c6d8a
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2021-10-13 14:42:23 +0200

    Correctly save config file

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,7 @@ use std::io::ErrorKind;
 use std::path::PathBuf;
 use tokio::fs;
 use toml;
+use tracing::debug;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
@@ -17,20 +18,29 @@ pub struct Config {
 impl Config {
     pub async fn load(path: Option<&str>) -> Result<Self, Error> {
         let path = parse_config_file_path(path)?;
+        debug!("loading config from: {}", path.as_path().display());
+
         match fs::read_to_string(&path).await {
             Ok(string) => toml::from_str(&string).map_err(|e| e.into()),
             // TODO should we create an empty file here if one does not already exist?
-            Err(err) if err.kind() == ErrorKind::NotFound => Ok(Config {
-                path,
-                api_token: None,
-            }),
+            Err(err) if err.kind() == ErrorKind::NotFound => {
+                debug!("no config file found, using default config");
+                Ok(Config {
+                    path,
+                    api_token: None,
+                })
+            }
             Err(err) => Err(err.into()),
         }
     }
 
     pub async fn save(&self) -> Result<(), Error> {
         let string = toml::to_string_pretty(&self)?;
+        if let Some(dir) = self.path.parent() {
+            fs::create_dir_all(dir).await?;
+        }
         fs::write(&self.path, string).await?;
+        debug!("saved config to: {}", self.path.as_path().display());
         Ok(())
     }
 }
@@ -54,5 +64,5 @@ fn default_config_file_path() -> PathBuf {
         .unwrap()
         .config_dir()
         .to_owned()
-        .with_file_name("config.toml")
+        .join("config.toml")
 }
