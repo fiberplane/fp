baf65f3c294cd0dbd33b6cc6706d30b96c214915
Benno van den Berg <benno@fiberplane.com>
2022-04-14 15:54:56 +0200

    Make proxies commands more consistent

--- src/proxies.rs
+++ src/proxies.rs
@@ -1,7 +1,7 @@
 use crate::config::api_client_configuration;
-use crate::output::{output_details, output_list, GenericKeyValue};
+use crate::output::{output_details, output_json, output_list, GenericKeyValue};
 use anyhow::{anyhow, Result};
-use clap::Parser;
+use clap::{ArgEnum, Parser};
 use cli_table::Table;
 use fp_api_client::apis::default_api::{
     proxy_create, proxy_data_sources_list, proxy_delete, proxy_get, proxy_list,
@@ -22,32 +22,31 @@ pub struct Arguments {
 #[derive(Parser)]
 pub enum SubCommand {
     /// Create a new Proxy
-    #[clap()]
     Create(CreateArgs),
 
     /// List all proxies
-    #[clap()]
-    List(GlobalArgs),
+    List(ListArgs),
 
     /// List all data sources
     #[clap(alias = "datasources")]
-    DataSources(GlobalArgs),
+    DataSources(DataSourcesArgs),
 
     /// Retrieve a single proxy
-    #[clap()]
-    Get(SingleProxyArgs),
+    Get(GetArgs),
 
     /// Remove a proxy
-    #[clap()]
-    Remove(SingleProxyArgs),
+    Remove(RemoveArgs),
 }
 
 #[derive(Parser)]
 pub struct CreateArgs {
     /// Proxy name, leave empty to auto-generate a name
-    #[clap()]
     name: Option<String>,
 
+    /// Output of the proxy
+    #[clap(long, short, default_value = "table", arg_enum)]
+    output: ProxyOutput,
+
     #[clap(from_global)]
     base_url: Url,
 
@@ -56,7 +55,11 @@ pub struct CreateArgs {
 }
 
 #[derive(Parser)]
-pub struct GlobalArgs {
+pub struct ListArgs {
+    /// Output of the proxy
+    #[clap(long, short, default_value = "table", arg_enum)]
+    output: ProxyOutput,
+
     #[clap(from_global)]
     base_url: Url,
 
@@ -65,9 +68,37 @@ pub struct GlobalArgs {
 }
 
 #[derive(Parser)]
-pub struct SingleProxyArgs {
+pub struct DataSourcesArgs {
+    /// Output of the proxy
+    #[clap(long, short, default_value = "table", arg_enum)]
+    output: ProxyOutput,
+
+    #[clap(from_global)]
+    base_url: Url,
+
+    #[clap(from_global)]
+    config: Option<PathBuf>,
+}
+
+#[derive(Parser)]
+pub struct GetArgs {
     /// ID of the proxy
-    #[clap()]
+    proxy_id: String,
+
+    /// Output of the proxy
+    #[clap(long, short, default_value = "table", arg_enum)]
+    output: ProxyOutput,
+
+    #[clap(from_global)]
+    base_url: Url,
+
+    #[clap(from_global)]
+    config: Option<PathBuf>,
+}
+
+#[derive(Parser)]
+pub struct RemoveArgs {
+    /// ID of the proxy
     proxy_id: String,
 
     #[clap(from_global)]
@@ -77,10 +108,20 @@ pub struct SingleProxyArgs {
     config: Option<PathBuf>,
 }
 
+/// A generic output for proxy related commands.
+#[derive(ArgEnum, Clone)]
+enum ProxyOutput {
+    /// Output the result as a table
+    Table,
+
+    /// Output the result as a JSON encoded object
+    Json,
+}
+
 pub async fn handle_command(args: Arguments) -> Result<()> {
     use SubCommand::*;
     match args.sub_command {
-        Create(args) => handle_add_command(args).await,
+        Create(args) => handle_create_command(args).await,
         List(args) => handle_list_command(args).await,
         Get(args) => handle_get_command(args).await,
         DataSources(args) => handle_data_sources_command(args).await,
@@ -88,7 +129,7 @@ pub async fn handle_command(args: Arguments) -> Result<()> {
     }
 }
 
-async fn handle_add_command(args: CreateArgs) -> Result<()> {
+async fn handle_create_command(args: CreateArgs) -> Result<()> {
     let name = args.name.unwrap_or_else(|| petname(2, "-"));
     let config = api_client_configuration(args.config, &args.base_url).await?;
 
@@ -96,56 +137,73 @@ async fn handle_add_command(args: CreateArgs) -> Result<()> {
         .await
         .map_err(|e| anyhow!(format!("Error adding proxy: {:?}", e)))?;
 
-    let token = proxy
-        .token
-        .clone()
-        .ok_or_else(|| anyhow!("Create proxy endpoint should have returned an API token"))?;
-
-    let mut proxy = GenericKeyValue::from_proxy(proxy);
-    proxy.push(GenericKeyValue::new("Token", token));
-
-    output_details(proxy)
+    match args.output {
+        ProxyOutput::Table => {
+            let token = proxy.token.clone().ok_or_else(|| {
+                anyhow!("Create proxy endpoint should have returned an API token")
+            })?;
+            let mut proxy = GenericKeyValue::from_proxy(proxy);
+            proxy.push(GenericKeyValue::new("Token", token));
+            output_details(proxy)
+        }
+        ProxyOutput::Json => output_json(&proxy),
+    }
 }
 
-async fn handle_list_command(args: GlobalArgs) -> Result<()> {
+async fn handle_list_command(args: ListArgs) -> Result<()> {
     let config = api_client_configuration(args.config, &args.base_url).await?;
     let mut proxies = proxy_list(&config).await?;
 
-    // Show connected proxies first, and then sort alphabetically by name
-    proxies.sort_by(|a, b| {
-        use fp_api_client::models::ProxyConnectionStatus::*;
-        match (a.status, b.status) {
-            (Connected, Disconnected) => Ordering::Less,
-            (Disconnected, Connected) => Ordering::Greater,
-            _ => a.name.cmp(&b.name),
+    match args.output {
+        ProxyOutput::Table => {
+            // Show connected proxies first, and then sort alphabetically by name
+            proxies.sort_by(|a, b| {
+                use fp_api_client::models::ProxyConnectionStatus::*;
+                match (a.status, b.status) {
+                    (Connected, Disconnected) => Ordering::Less,
+                    (Disconnected, Connected) => Ordering::Greater,
+                    _ => a.name.cmp(&b.name),
+                }
+            });
+
+            let proxies: Vec<ProxySummaryRow> = proxies.into_iter().map(Into::into).collect();
+
+            output_list(proxies)
         }
-    });
-
-    let proxies: Vec<ProxySummaryRow> = proxies.into_iter().map(Into::into).collect();
-
-    output_list(proxies)
+        ProxyOutput::Json => output_json(&proxies),
+    }
 }
 
-async fn handle_get_command(args: SingleProxyArgs) -> Result<()> {
+async fn handle_get_command(args: GetArgs) -> Result<()> {
     let config = api_client_configuration(args.config, &args.base_url).await?;
     let proxy = proxy_get(&config, &args.proxy_id).await?;
 
-    let proxy = GenericKeyValue::from_proxy(proxy);
+    match args.output {
+        ProxyOutput::Table => {
+            let proxy = GenericKeyValue::from_proxy(proxy);
 
-    output_details(proxy)
+            output_details(proxy)
+        }
+        ProxyOutput::Json => output_json(&proxy),
+    }
 }
 
-async fn handle_data_sources_command(args: GlobalArgs) -> Result<()> {
+async fn handle_data_sources_command(args: DataSourcesArgs) -> Result<()> {
     let config = api_client_configuration(args.config, &args.base_url).await?;
     let data_sources = proxy_data_sources_list(&config).await?;
 
-    let data_sources: Vec<DataSourceAndProxySummaryRow> =
-        data_sources.into_iter().map(Into::into).collect();
+    match args.output {
+        ProxyOutput::Table => {
+            let data_sources: Vec<DataSourceAndProxySummaryRow> =
+                data_sources.into_iter().map(Into::into).collect();
 
-    output_list(data_sources)
+            output_list(data_sources)
+        }
+        ProxyOutput::Json => output_json(&data_sources),
+    }
 }
 
-async fn handle_remove_command(args: SingleProxyArgs) -> Result<()> {
+async fn handle_remove_command(args: RemoveArgs) -> Result<()> {
     let config = api_client_configuration(args.config, &args.base_url).await?;
     proxy_delete(&config, &args.proxy_id).await?;
     info!("Removed proxy");
