e76727bc246626570ac200dbc53f6c60d128dd9d
Simon Rasmussen <zetlon@gmail.com>
2022-06-24 16:04:47 +0200

    Rip out custom future

--- src/shell/notebook_writer.rs
+++ src/shell/notebook_writer.rs
@@ -1,28 +1,18 @@
 use anyhow::{anyhow, Result};
+use fp_api_client::apis::configuration::Configuration;
 use fp_api_client::apis::default_api::{
-    get_profile, notebook_cell_append_text, notebook_cells_append, NotebookCellAppendTextError,
+    get_profile, notebook_cell_append_text, notebook_cells_append,
 };
-use fp_api_client::apis::{configuration::Configuration, Error};
 use fp_api_client::models::{cell::HeadingType, Annotation, Cell, CellAppendText};
 use once_cell::sync::OnceCell;
-use pin_project::pin_project;
-use std::{future::Future, pin::Pin, sync::Arc, task::Poll};
 use time::{format_description::FormatItem, OffsetDateTime};
 
-struct Inner {
+pub struct NotebookWriter {
     config: Configuration,
     notebook_id: String,
     code_cell_id: String,
     heading_cell_id: String,
-}
-
-#[pin_project]
-pub struct NotebookWriter {
-    #[pin]
-    inner: Arc<Inner>,
-    #[pin]
     buffer: Vec<u8>,
-    future: Option<Pin<Box<dyn Future<Output = Result<Cell, Error<NotebookCellAppendTextError>>>>>>,
 }
 
 fn get_ts_format() -> &'static (impl time::formatting::Formattable + ?Sized) {
@@ -85,14 +75,11 @@ impl NotebookWriter {
         };
 
         Ok(Self {
-            inner: Arc::new(Inner {
-                config,
-                notebook_id,
-                code_cell_id,
-                heading_cell_id,
-            }),
-            buffer: Vec::with_capacity(4096),
-            future: None,
+            config,
+            notebook_id,
+            code_cell_id,
+            heading_cell_id,
+            buffer: Vec::with_capacity(256),
         })
     }
 
@@ -100,11 +87,28 @@ impl NotebookWriter {
         self.buffer.is_empty()
     }
 
+    pub async fn flush(&mut self) -> Result<()> {
+        let content = String::from_utf8_lossy(&self.buffer).to_string();
+
+        notebook_cell_append_text(
+            &self.config,
+            &self.notebook_id,
+            &self.code_cell_id,
+            CellAppendText {
+                content,
+                formatting: None,
+            },
+        )
+        .await?;
+        self.buffer.clear();
+        Ok(())
+    }
+
     pub async fn close(&self) -> Result<()> {
         notebook_cell_append_text(
-            &self.inner.config,
-            &self.inner.notebook_id,
-            &self.inner.heading_cell_id,
+            &self.config,
+            &self.notebook_id,
+            &self.heading_cell_id,
             CellAppendText {
                 content: format!(
                     "\n🔴 Ended at:\t{}",
@@ -115,62 +119,5 @@ impl NotebookWriter {
         )
         .await?;
         Ok(())
-    }
-}
-
-impl tokio::io::AsyncWrite for NotebookWriter {
-    fn poll_write(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-        buf: &[u8],
-    ) -> std::task::Poll<Result<usize, std::io::Error>> {
-        let this = self.project();
-        tokio::io::AsyncWrite::poll_write(this.buffer, cx, buf)
-    }
-
-    fn poll_flush(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), std::io::Error>> {
-        let this = self.project();
-
-        let inner = this.inner.clone();
-        let buf = this.buffer.get_mut();
-
-        let fut = this.future.get_or_insert_with(|| {
-            let content = String::from_utf8_lossy(buf).to_string();
-            buf.clear();
-
-            Box::pin(async move {
-                let inner = inner;
-
-                notebook_cell_append_text(
-                    &inner.config,
-                    &inner.notebook_id,
-                    &inner.code_cell_id,
-                    CellAppendText {
-                        content,
-                        formatting: None,
-                    },
-                )
-                .await
-            })
-        });
-
-        match fut.as_mut().poll(cx) {
-            Poll::Ready(_res) => {
-                *this.future = None; //future is done polling so we eat it
-                Poll::Ready(Ok(()))
-            }
-            Poll::Pending => Poll::Pending,
-        }
-    }
-
-    fn poll_shutdown(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), std::io::Error>> {
-        let this = self.project();
-        tokio::io::AsyncWrite::poll_shutdown(this.buffer, cx)
     }
 }
