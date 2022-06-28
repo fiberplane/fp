91f29a31b3ea8d75c5b5aa9b77e1e56709d79170
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2022-06-28 15:22:39 +0200

    Move exec command out of experiments (#98)

--- src/experiments.rs
+++ src/experiments.rs
@@ -1,28 +1,20 @@
 use crate::config::api_client_configuration;
 use crate::output::{output_details, output_json, GenericKeyValue};
 use crate::templates::NOTEBOOK_ID_REGEX;
-use anyhow::{anyhow, Context, Error, Result};
-use bytes::Bytes;
+use anyhow::{anyhow, Context, Result};
 use clap::{ArgEnum, Parser};
 use directories::ProjectDirs;
 use fiberplane::protocols::{core, formatting};
 use fiberplane_markdown::notebook_to_markdown;
-use fp_api_client::apis::configuration::Configuration;
-use fp_api_client::apis::default_api::{
-    get_notebook, get_profile, notebook_cell_append_text, notebook_cells_append,
-};
-use fp_api_client::models::{Annotation, Cell, CellAppendText};
-use futures::StreamExt;
+use fp_api_client::apis::default_api::{get_notebook, get_profile, notebook_cells_append};
+use fp_api_client::models::{Annotation, Cell};
 use lazy_static::lazy_static;
 use regex::{Regex, Replacer};
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, VecDeque};
-use std::{env::current_dir, io::ErrorKind, path::PathBuf, process::Stdio, str::FromStr};
-use std::{fmt::Write, time::Duration};
+use std::{fmt::Write, io::ErrorKind, path::PathBuf, str::FromStr};
 use time::{format_description::well_known::Rfc3339, OffsetDateTime};
-use tokio::io::{self, AsyncWriteExt};
-use tokio::{fs, process::Command, signal, time::interval};
-use tokio_util::io::ReaderStream;
+use tokio::fs;
 use tracing::{debug, info, warn};
 use url::Url;
 
@@ -41,9 +33,6 @@ pub struct Arguments {
 enum SubCommand {
     /// Append a message to the given notebook
     Message(MessageArgs),
-
-    /// Execute a shell command and pipe the output to a notebook
-    Exec(ExecArgs),
 
     /// Starting with the given notebook, recursively crawl all linked notebooks
     /// and save them to the given directory as Markdown
@@ -68,29 +57,6 @@ struct MessageArgs {
     /// Output type to display
     #[clap(long, short, default_value = "table", arg_enum)]
     output: MessageOutput,
-}
-
-#[derive(Parser, Clone)]
-struct ExecArgs {
-    /// The notebook to append the message to
-    #[clap(long, short, env)]
-    notebook_id: String,
-
-    /// The command to run
-    command: String,
-
-    /// Args to pass to the command
-    args: Vec<String>,
-
-    #[clap(from_global)]
-    base_url: Url,
-
-    #[clap(from_global)]
-    config: Option<PathBuf>,
-
-    /// Output type to display
-    #[clap(long, short, default_value = "command", arg_enum)]
-    output: ExecOutput,
 }
 
 #[derive(Parser)]
@@ -119,22 +85,9 @@ enum MessageOutput {
     Json,
 }
 
-#[derive(ArgEnum, Clone, PartialEq)]
-enum ExecOutput {
-    /// Output the result of the command
-    Command,
-
-    /// Output the cell details as a table
-    Table,
-
-    /// Output the cell details as a JSON encoded object
-    Json,
-}
-
 pub async fn handle_command(args: Arguments) -> Result<()> {
     match args.sub_command {
         SubCommand::Message(args) => handle_message_command(args).await,
-        SubCommand::Exec(args) => handle_exec_command(args).await,
         SubCommand::Crawl(args) => handle_crawl_command(args).await,
     }
 }
@@ -188,176 +141,6 @@ async fn handle_message_command(args: MessageArgs) -> Result<()> {
 }
 
 /// This buffers text to be written to a notebook cell
-struct CellWriter {
-    args: ExecArgs,
-    config: Configuration,
-    cell: Option<core::Cell>,
-    buffer: Vec<Bytes>,
-}
-
-impl CellWriter {
-    pub fn new(args: ExecArgs, config: Configuration) -> Self {
-        Self {
-            args,
-            config,
-            cell: None,
-            buffer: Vec::new(),
-        }
-    }
-
-    pub fn append(&mut self, data: Bytes) {
-        self.buffer.push(data);
-    }
-
-    /// Create a new cell and write the buffered text to it
-    /// or append the buffered text to the cell if one was
-    /// already created
-    pub async fn write_to_cell(&mut self) -> Result<()> {
-        if self.buffer.is_empty() {
-            return Ok(());
-        }
-
-        let mut output = String::new();
-        let buffer = self.buffer.split_off(0);
-        for chunk in buffer {
-            output.push_str(&String::from_utf8_lossy(&chunk));
-        }
-
-        // Either create a new cell or append to the existing one
-        match &mut self.cell {
-            None => {
-                let timestamp = OffsetDateTime::now_utc().format(&Rfc3339)?;
-                let cwd = current_dir()
-                    .map(|p| p.display().to_string())
-                    .unwrap_or_default();
-                let content = format!(
-                    "{}\n{} ❯ {} {}\n{}",
-                    timestamp,
-                    cwd,
-                    self.args.command,
-                    self.args.args.join(" "),
-                    output
-                );
-                let cell = Cell::CodeCell {
-                    id: String::new(),
-                    content,
-                    syntax: None,
-                    read_only: None,
-                };
-
-                let cell = notebook_cells_append(&self.config, &self.args.notebook_id, vec![cell])
-                    .await
-                    .with_context(|| "Error appending cell to notebook")?
-                    .pop()
-                    .ok_or_else(|| anyhow!("No cells returned"))?;
-                self.cell = Some(serde_json::from_value(serde_json::to_value(cell)?)?);
-            }
-            Some(cell) => {
-                notebook_cell_append_text(
-                    &self.config,
-                    &self.args.notebook_id,
-                    cell.id(),
-                    CellAppendText {
-                        content: output,
-                        formatting: None,
-                    },
-                )
-                .await
-                .with_context(|| format!("Error appending text to cell {}", cell.id()))?;
-            }
-        }
-        Ok::<_, Error>(())
-    }
-
-    pub fn into_output_cell(self) -> Option<core::Cell> {
-        self.cell
-    }
-}
-
-async fn handle_exec_command(args: ExecArgs) -> Result<()> {
-    debug!("Running command: \"{}\"", args.command);
-    let config = api_client_configuration(args.config.clone(), &args.base_url).await?;
-    let mut child = Command::new(&args.command)
-        .args(&args.args)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .stdin(Stdio::inherit())
-        .spawn()
-        .with_context(|| "Error spawning child process to run command")?;
-
-    let mut child_stdout = ReaderStream::new(child.stdout.take().unwrap());
-    let mut child_stderr = ReaderStream::new(child.stderr.take().unwrap());
-    let mut stdout = io::stdout();
-    let mut stderr = io::stderr();
-
-    let mut cell_writer = CellWriter::new(args.clone(), config);
-
-    // Read from the child process' stdout/stderr and write the output to the notebook
-    // cell every 250 milliseconds
-    let mut send_interval = interval(Duration::from_millis(250));
-    loop {
-        tokio::select! {
-            biased;
-            // This sets up a ctrl-c handler so that the output will be written even if the process is killed
-            // (This is important when using this command with a long-running command that needs to be
-            // exited manually but where you still want to see the ouput)
-            _ = signal::ctrl_c() => {
-                break;
-            }
-            _ = child.wait() => {
-                cell_writer.write_to_cell().await?;
-                break;
-            }
-            _ = send_interval.tick() => {
-                cell_writer.write_to_cell().await?;
-            }
-            chunk = child_stdout.next() => {
-                if let Some(Ok(chunk)) = chunk {
-                    if args.output == ExecOutput::Command {
-                        stdout.write_all(&chunk).await?;
-                    }
-                    cell_writer.append(chunk);
-                }
-            }
-            chunk = child_stderr.next() => {
-                if let Some(Ok(chunk)) = chunk {
-                    if args.output == ExecOutput::Command {
-                        stderr.write_all(&chunk).await?;
-                    }
-                    cell_writer.append(chunk);
-                }
-            }
-        }
-    }
-
-    let cell = cell_writer.into_output_cell();
-    let mut url = args
-        .base_url
-        .join("/notebook/")
-        .unwrap()
-        .join(&args.notebook_id)
-        .unwrap();
-    if let Some(cell) = &cell {
-        url.set_fragment(Some(cell.id()));
-    };
-
-    if let Some(cell) = &cell {
-        let cell: Cell = serde_json::from_value(serde_json::to_value(cell)?)?;
-        match args.output {
-            ExecOutput::Command => {
-                info!("\n   --> Created cell: {}", url);
-                Ok(())
-            }
-            ExecOutput::Table => {
-                info!("Created cell");
-                output_details(GenericKeyValue::from_cell(cell))
-            }
-            ExecOutput::Json => output_json(&cell),
-        }
-    } else {
-        Ok(())
-    }
-}
 
 struct NotebookUrlReplacer<'a>(&'a HashMap<String, CrawledNotebook>);
 
