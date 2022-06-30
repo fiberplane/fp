ba259954dabbe582f35febfb87ac78b724037f5b
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2022-06-30 21:01:46 +0200

    Use Grok to parse Nginx logs

--- src/exec/parse_logs.rs
+++ src/exec/parse_logs.rs
@@ -1,8 +1,12 @@
 use fp_api_client::models::LogRecord;
-use serde::Deserialize;
+use grok::{Grok, Pattern};
+use once_cell::unsync::Lazy;
+use serde::{de::Error, Deserialize, Deserializer};
 use serde_json::{Map, Value};
 use std::collections::HashMap;
-use time::{format_description::well_known::Rfc3339, OffsetDateTime};
+use time::format_description::{well_known::Rfc3339, FormatItem};
+use time::{macros::format_description, OffsetDateTime};
+use tracing::warn;
 
 pub(crate) static TIMESTAMP_FIELDS: &[&str] = &["@timestamp", "timestamp", "fields.timestamp"];
 pub(crate) static BODY_FIELDS: &[&str] =
@@ -12,6 +16,9 @@ pub(crate) static BODY_FIELDS: &[&str] =
 // https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/logs/data-model.md#elastic-common-schema
 static RESOURCE_FIELD_PREFIXES: &[&str] = &["agent.", "cloud.", "container.", "host.", "service."];
 static RESOURCE_FIELD_EXCEPTIONS: &[&str] = &["container.labels", "host.uptime", "service.state"];
+
+static NGINX_GROK: &str = r#"%{IPORHOST:clientip} %{USER:ident} %{USER:auth} \[%{HTTPDATE:timestamp}\] "(?:%{WORD:verb} %{NOTSPACE:request}(?: HTTP/%{NUMBER:httpversion})?|%{DATA:rawrequest})" %{NUMBER:response} (?:%{NUMBER:bytes}|-) %{QS:referrer} %{QS:agent}"#;
+const NGINX_TIMESTAMP_FORMAT: &[FormatItem] = format_description!("[day]/[month repr:short]/[year]:[hour repr:24]:[minute]:[second] [offset_hour sign:mandatory][offset_minute]");
 
 #[derive(Deserialize, Debug)]
 #[serde(untagged)]
@@ -24,6 +31,16 @@ enum AnyTimestamp {
     Iso8601(OffsetDateTime),
     #[serde(with = "time::serde::rfc2822")]
     Rfc2822(OffsetDateTime),
+    #[serde(deserialize_with = "deserialize_nginx_timestamp")]
+    Nginx(OffsetDateTime),
+}
+
+pub fn deserialize_nginx_timestamp<'de, D>(deserializer: D) -> Result<OffsetDateTime, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    OffsetDateTime::parse(&s, NGINX_TIMESTAMP_FORMAT).map_err(D::Error::custom)
 }
 
 impl From<AnyTimestamp> for OffsetDateTime {
@@ -33,6 +50,7 @@ impl From<AnyTimestamp> for OffsetDateTime {
             AnyTimestamp::Rfc3339(t) => t,
             AnyTimestamp::Iso8601(t) => t,
             AnyTimestamp::Rfc2822(t) => t,
+            AnyTimestamp::Nginx(t) => t,
         }
     }
 }
@@ -40,14 +58,28 @@ impl From<AnyTimestamp> for OffsetDateTime {
 pub fn parse_logs(output: &str) -> HashMap<String, Vec<LogRecord>> {
     let mut logs: HashMap<String, Vec<LogRecord>> = HashMap::new();
 
+    let nginx: Lazy<Pattern> = Lazy::new(|| Grok::default().compile(NGINX_GROK, false).unwrap());
+
     for line in output.lines() {
-        if let Ok(Value::Object(json)) = serde_json::from_str(line) {
-            if let Some((timestamp, log)) = parse_json(json) {
-                if let Some(records) = logs.get_mut(&timestamp) {
-                    records.push(log);
-                } else {
-                    logs.insert(timestamp, vec![log]);
-                }
+        let result = if let Ok(Value::Object(json)) = serde_json::from_str(line) {
+            parse_json(json)
+        } else if let Some(matches) = nginx.match_against(line) {
+            let fields = matches
+                .into_iter()
+                .filter(|(k, _)| k.chars().all(|c| c.is_lowercase()))
+                .map(|(k, v)| (k.to_string(), v.trim_matches('"').to_string()))
+                .collect();
+            parse_flattened_json(fields)
+        } else {
+            warn!("Unable to parse log line: {}", line);
+            continue;
+        };
+
+        if let Some((timestamp, log)) = result {
+            if let Some(records) = logs.get_mut(&timestamp) {
+                records.push(log);
+            } else {
+                logs.insert(timestamp, vec![log]);
             }
         }
     }
@@ -59,7 +91,12 @@ fn parse_json(json: Map<String, Value>) -> Option<(String, LogRecord)> {
     for (key, val) in json.into_iter() {
         flatten_nested_value(&mut flattened_fields, key, val);
     }
+    parse_flattened_json(flattened_fields)
+}
 
+fn parse_flattened_json(
+    mut flattened_fields: HashMap<String, String>,
+) -> Option<(String, LogRecord)> {
     let trace_id = flattened_fields
         .remove("trace_id")
         .or(flattened_fields.remove("trace.id"));
