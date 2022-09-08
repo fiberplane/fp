d553d34f2aa3b9ac9e73827dedc5ff2ce4deda73
Mari <git@cutegirl.tech>
2022-09-08 13:49:29 +0200

    add workspace leave command

--- src/workspaces.rs
+++ src/workspaces.rs
@@ -3,7 +3,7 @@ use crate::interactive::workspace_picker;
 use anyhow::Result;
 use base64uuid::Base64Uuid;
 use clap::Parser;
-use fp_api_client::apis::default_api::workspace_invite;
+use fp_api_client::apis::default_api::{workspace_invite, workspace_leave};
 use fp_api_client::models::NewWorkspaceInvite;
 use std::path::PathBuf;
 use tracing::info;
@@ -19,6 +19,9 @@ pub struct Arguments {
 enum SubCommand {
     /// Invite a user to a workspace
     Invite(InviteArgs),
+
+    /// Leave a workspace
+    Leave(LeaveArgs),
 }
 
 #[derive(Parser)]
@@ -38,9 +41,23 @@ struct InviteArgs {
     config: Option<PathBuf>,
 }
 
+#[derive(Parser)]
+struct LeaveArgs {
+    /// Workspace to leave from
+    #[clap(long, short, env)]
+    workspace_id: Option<Base64Uuid>,
+
+    #[clap(from_global)]
+    base_url: Url,
+
+    #[clap(from_global)]
+    config: Option<PathBuf>,
+}
+
 pub async fn handle_command(args: Arguments) -> Result<()> {
     match args.sub_command {
         SubCommand::Invite(args) => handle_workspace_invite(args).await,
+        SubCommand::Leave(args) => handle_workspace_leave(args).await,
     }
 }
 
@@ -56,5 +73,15 @@ async fn handle_workspace_invite(args: InviteArgs) -> Result<()> {
     .await?;
 
     info!("Successfully invited user to workspace");
+    Ok(())
+}
+
+async fn handle_workspace_leave(args: LeaveArgs) -> Result<()> {
+    let config = api_client_configuration(args.config, &args.base_url).await?;
+    let workspace_id = workspace_picker(&config, args.workspace_id).await?;
+
+    workspace_leave(&config, &workspace_id.to_string()).await?;
+
+    info!("Successfully left workspace");
     Ok(())
 }
