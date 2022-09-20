614ff4b4059633e4cae93fca9a5f077dfb6fc68f
Mari <git@cutegirl.tech>
2022-09-20 16:13:16 +0200

    add commands to update workspace owner and name

--- src/workspaces.rs
+++ src/workspaces.rs
@@ -7,10 +7,12 @@ use clap::{ArgEnum, Parser};
 use cli_table::Table;
 use fiberplane::sorting::{SortDirection, WorkspaceInviteListingSortFields};
 use fp_api_client::apis::default_api::{
-    workspace_create, workspace_invite, workspace_invite_get, workspace_leave,
+    workspace_create, workspace_invite, workspace_invite_get, workspace_leave, workspace_update,
     workspace_user_remove,
 };
-use fp_api_client::models::{NewWorkspace, NewWorkspaceInvite, Workspace, WorkspaceInvite};
+use fp_api_client::models::{
+    NewWorkspace, NewWorkspaceInvite, UpdateWorkspace, Workspace, WorkspaceInvite,
+};
 use std::path::PathBuf;
 use tracing::info;
 use url::Url;
@@ -37,6 +39,9 @@ enum SubCommand {
 
     /// Remove a user from a workspace
     Remove(RemoveArgs),
+
+    /// Update workspace settings
+    Update(UpdateArgs),
 }
 
 #[derive(ArgEnum, Clone)]
@@ -154,6 +159,63 @@ struct RemoveArgs {
     config: Option<PathBuf>,
 }
 
+#[derive(Parser)]
+struct UpdateArgs {
+    /// Workspace to update settings on
+    #[clap(long, short, env, global = true)]
+    workspace_id: Option<Base64Uuid>,
+
+    #[clap(subcommand)]
+    sub_command: UpdateSubCommand,
+
+    #[clap(from_global)]
+    base_url: Url,
+
+    #[clap(from_global)]
+    config: Option<PathBuf>,
+}
+
+#[derive(Parser)]
+enum UpdateSubCommand {
+    /// Move ownership of workspace to new owner
+    Owner(MoveOwnerArgs),
+
+    /// Change name of workspace
+    Name(ChangeNameArgs),
+}
+
+#[derive(Parser)]
+struct MoveOwnerArgs {
+    /// ID of the member who should become workspace owner
+    #[clap(long, short = 'o', env)]
+    new_owner_id: Option<Base64Uuid>,
+
+    #[clap(from_global)]
+    workspace_id: Option<Base64Uuid>,
+
+    #[clap(from_global)]
+    base_url: Url,
+
+    #[clap(from_global)]
+    config: Option<PathBuf>,
+}
+
+#[derive(Parser)]
+struct ChangeNameArgs {
+    /// New name for the workspace
+    #[clap(long, short = 'n', env)]
+    new_name: String,
+
+    #[clap(from_global)]
+    workspace_id: Option<Base64Uuid>,
+
+    #[clap(from_global)]
+    base_url: Url,
+
+    #[clap(from_global)]
+    config: Option<PathBuf>,
+}
+
 pub async fn handle_command(args: Arguments) -> Result<()> {
     match args.sub_command {
         SubCommand::Create(args) => handle_workspace_create(args).await,
@@ -161,6 +223,10 @@ pub async fn handle_command(args: Arguments) -> Result<()> {
         SubCommand::ListInvites(args) => handle_list_invites(args).await,
         SubCommand::Leave(args) => handle_workspace_leave(args).await,
         SubCommand::Remove(args) => handle_workspace_remove_user(args).await,
+        SubCommand::Update(args) => match args.sub_command {
+            UpdateSubCommand::Owner(args) => handle_move_owner(args).await,
+            UpdateSubCommand::Name(args) => handle_change_name(args).await,
+        },
     }
 }
 
@@ -234,6 +300,44 @@ async fn handle_workspace_remove_user(args: RemoveArgs) -> Result<()> {
     workspace_user_remove(&config, &workspace_id.to_string(), &user.to_string()).await?;
 
     info!("Successfully removed user from workspace");
+    Ok(())
+}
+
+async fn handle_move_owner(args: MoveOwnerArgs) -> Result<()> {
+    let config = api_client_configuration(args.config, &args.base_url).await?;
+    let workspace_id = workspace_picker(&config, args.workspace_id).await?;
+
+    let new_owner = workspace_user_picker(&config, &workspace_id, args.new_owner_id).await?;
+
+    workspace_update(
+        &config,
+        &workspace_id.to_string(),
+        UpdateWorkspace {
+            title: None,
+            owner: Some(new_owner.to_string()),
+        },
+    )
+    .await?;
+
+    info!("Successfully moved ownership of workspace");
+    Ok(())
+}
+
+async fn handle_change_name(args: ChangeNameArgs) -> Result<()> {
+    let config = api_client_configuration(args.config, &args.base_url).await?;
+    let workspace_id = workspace_picker(&config, args.workspace_id).await?;
+
+    workspace_update(
+        &config,
+        &workspace_id.to_string(),
+        UpdateWorkspace {
+            title: Some(args.new_name),
+            owner: None,
+        },
+    )
+    .await?;
+
+    info!("Successfully changed name of workspace");
     Ok(())
 }
 
